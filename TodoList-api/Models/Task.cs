6f127683b0f66d3ceba30a8f492e9b6d00fd8dc8
marcosllbs <marcosllbs@hotmail.com>
2024-11-01 12:53:41 -0300

    fix required description

--- TodoList-api/Models/Task.cs
+++ TodoList-api/Models/Task.cs
@@ -6,7 +6,7 @@ public class Task
     [Key]
     public int TaskId { get; set; }
 
-    [Required]
+
     [MinLength(5, ErrorMessage = "A descrição deve ter pelo menos 5 caracteres.")]
     [MaxLength(100, ErrorMessage = "A descrição deve ter no maximo 100 caracteres.")]
     public string? Description { get; set; }
