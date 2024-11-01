856e1e15d6cf21901d439b437f1964d1f47ce536
marcosllbs <marcosllbs@hotmail.com>
2024-11-01 12:08:52 -0300

    End Points Status

--- TodoList-api/Controllers/TaskController.cs
+++ TodoList-api/Controllers/TaskController.cs
@@ -12,24 +12,32 @@ public class TaskController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult GetTasks()
     {
         return Ok(_repository.GetTasks());
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(Task), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult AddTask([FromBody] Task task)
     {
         return Created("", _repository.AddTask(task));
     }
 
     [HttpPut("{taskId:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult UpdateTask([FromBody] Task task, int taskId)
     {
         return Ok(_repository.UpDateTask(task, taskId));
     }
 
     [HttpDelete("{taskId:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult DeleteTask(int taskId)
     {
         _repository.DeleteTask(taskId);
@@ -37,6 +45,8 @@ public class TaskController : ControllerBase
     }
 
     [HttpPut("{taskId:int}/finishtask")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult FinishTask(int taskId)
     {
         return NoContent();
