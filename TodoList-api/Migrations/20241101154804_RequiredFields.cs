cb26ee95957f6149042fe8644d7e2a77d309f2c7
marcosllbs <marcosllbs@hotmail.com>
2024-11-01 12:50:34 -0300

    Fix required fields on entity

---
+++ TodoList-api/Migrations/20241101154804_RequiredFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequiredFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
--- TodoList-api/Models/Task.cs
+++ TodoList-api/Models/Task.cs
@@ -5,7 +5,12 @@ public class Task
 
     [Key]
     public int TaskId { get; set; }
+
+    [Required]
+    [MinLength(5, ErrorMessage = "A descrição deve ter pelo menos 5 caracteres.")]
+    [MaxLength(100, ErrorMessage = "A descrição deve ter no maximo 100 caracteres.")]
     public string? Description { get; set; }
+
     public bool Done { get; set; }
     public DateTime Created_AT { get; set; } = DateTime.Now;
     public DateTime? Updated_AT { get; set; } = null;
